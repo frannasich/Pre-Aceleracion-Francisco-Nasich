ca44a0c3d3b09bf362816f966eee893995c6df7c
Francisco Nasich <frannasich@gmail.com>
2022-02-26 15:47:10 -0300

    Initial Commit

---
+++ Movie App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie App
+//
+//  Created by Fran Nasich on 26/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
