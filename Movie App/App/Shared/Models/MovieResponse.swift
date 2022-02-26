8a6531c46c8b79843db57473917a24fa348c835b
Francisco Nasich <frannasich@gmail.com>
2022-02-26 16:22:34 -0300

    .

---
+++ Movie App/App/Shared/Models/MovieResponse.swift
@@ -0,0 +1,8 @@
+//
+//  MovieResponse.swift
+//  Movie App
+//
+//  Created by Fran Nasich on 26/02/2022.
+//
+
+import Foundation
---
+++ Movie App/App/Shared/Networking/ApiManager.swift
@@ -0,0 +1,20 @@
+//
+//  ApiManager.swift
+//  Movie App
+//
+//  Created by Fran Nasich on 26/02/2022.
+//
+
+import Foundation
+import Alamofire
+
+class ApiManager {
+    static let shared = ApiManager()
+    
+    func get(url: String, completion: @escaping (Result<Data?, AFError>)) -> Void {
+        AF.request(url).response { response in
+            completion(response.result)
+        }
+        
+    }
+}
